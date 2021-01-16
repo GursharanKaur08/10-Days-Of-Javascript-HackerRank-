0a15b331d6d3f077da2432193599520f5dbd7a28
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-01-16 23:49:56 +0530

    Update Day 7: Regular Expressions I.js

--- Day 7: Regular Expressions I.js
+++ Day 7: Regular Expressions I.js
@@ -30,7 +30,6 @@ function regexVar()
      * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
      */
     let re =  /^([aeiou]).*\1$/;    
-    
     return re;
 }
 
