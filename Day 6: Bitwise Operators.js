f40e26770d4444e9529340434f1ff638464dac10
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-01-16 23:48:03 +0530

    Update Day 6: Bitwise Operators.js

--- Day 6: Bitwise Operators.js
+++ Day 6: Bitwise Operators.js
@@ -18,15 +18,20 @@ process.stdin.on('end', _ => {
     main();    
 });
 
-function readLine() {
+function readLine() 
+{
     return inputString[currentLine++];
 }
 
-function getMaxLessThanK(n, k) {
+function getMaxLessThanK(n, k) 
+{
     let max_anb = 0;
-    for (let b = n; b > 0; b--) {
-        for (let a = b-1; a > 0; a--) {
-            if ((a & b) < k && (a & b) > max_anb){
+    for (let b = n; b > 0; b--) 
+    {
+        for (let a = b-1; a > 0; a--) 
+        {
+            if ((a & b) < k && (a & b) > max_anb)
+            {
                 max_anb = (a&b);
             }
         }
@@ -35,10 +40,12 @@ function getMaxLessThanK(n, k) {
 }
 
 
-function main() {
+function main() 
+{
     const q = +(readLine());
     
-    for (let i = 0; i < q; i++) {
+    for (let i = 0; i < q; i++) 
+    {
         const [n, k] = readLine().split(' ').map(Number);
         
         console.log(getMaxLessThanK(n, k));
